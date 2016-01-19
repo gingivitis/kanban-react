cbd0047c0964115851d000032a5949eeaa2f595f
saliya seneviratne <sseneviratne@thinkful.com>
2016-01-18 21:50:52 -0500

    update client side routes to /api/*

--- src/utils/KanBanWebAPI.js
+++ src/utils/KanBanWebAPI.js
@@ -2,27 +2,27 @@ import axios from 'axios'
 
 class KanBanWebAPI {
     static getProject() {
-        return axios.get('/project')
+        return axios.get('/api/project')
     }
 
     static editProject(params) {
-        return axios.put('/project', params)
+        return axios.put('/api/project', params)
     }
 
     static fetchTasks() {
-        return axios.get('/tasks')
+        return axios.get('/api/tasks')
     }
 
     static updateTask(id, params) {
-        return axios.put(`/tasks/${id}`, params)
+        return axios.put(`/api/tasks/${id}`, params)
     }
 
     static addTask(params) {
-        return axios.post('/tasks', params)
+        return axios.post('/api/tasks', params)
     }
 
     static deleteTask(id) {
-        return axios.delete(`/tasks/${id}`)
+        return axios.delete(`/api/tasks/${id}`)
     }
 }
 
