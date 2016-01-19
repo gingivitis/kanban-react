51615fe35947a45256a33752fea33afa5de69df6
saliya seneviratne <sseneviratne@thinkful.com>
2016-01-18 21:49:35 -0500

    updated server cors and api prefix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const path = require('path')
 const express = require('express')
+const compression = require('compression')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const webpack = require('webpack')
-const config = require('./webpack.config.dev')
 const axios = require('axios')
 
 const API_BASE_URL = 'https://www.pivotaltracker.com/services/v5'
@@ -10,12 +11,16 @@ const USER_TOKEN = process.env.USER_TOKEN || exit('Must specify USER_TOKEN env v
 const PROJECT_ID = process.env.PROJECT_ID || exit('Must specify PROJECT_ID env variable')
 
 const app = express()
-const compiler = webpack(config)
+const staticPath = path.join(__dirname, './../build')
+
+app.enable('trust proxy')
 
 const request = axios.create({
     baseURL: API_BASE_URL,
     timeout: 2000,
-    headers: { 'X-TrackerToken': USER_TOKEN }
+    headers: {
+        'X-TrackerToken': USER_TOKEN
+    }
 })
 
 function exit(msg) {
@@ -25,59 +30,69 @@ function exit(msg) {
 
 app.use(bodyParser.json())
 
-app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-}))
+app.use(compression())
 
-app.use(require('webpack-hot-middleware')(compiler))
+app.options('/api', cors())
 
-
-app.get('/project', (req, res) => {
+app.get('/api/project', cors(), (req, res) => {
     request.get('/projects/' + PROJECT_ID)
         .then((response) => res.json(response.data))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.put('/project', (req, res) => {
+app.put('/api/project', cors(), (req, res) => {
     request.put('/projects/' + PROJECT_ID, req.body)
         .then((response) => res.json(response.data))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.post('/tasks', (req, res) => {
+app.post('/api/tasks', cors(), (req, res) => {
     request.post('/projects/' + PROJECT_ID + '/stories', req.body)
         .then((response) => res.json(response.data))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.get('/tasks', (req, res) => {
+app.get('/api/tasks', cors(), (req, res) => {
     request.get('/projects/' + PROJECT_ID + '/stories')
         .then((response) => res.json(response.data))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.put('/tasks/:id', (req, res) => {
+app.put('/api/tasks/:id', cors(), (req, res) => {
     request.put('/projects/' + PROJECT_ID + '/stories/' + req.params.id, req.body)
         .then((response) => res.json(response.data))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/api/tasks/:id', cors(), (req, res) => {
     request.delete('/projects/' + PROJECT_ID + '/stories/' + req.params.id)
         .then((response) => res.json(req.params))
         .catch((response) => res.status(response.status).json(response.data.error))
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.route('/').get(function(req, res) {
+    res.header('Cache-Control', "max-age=60, must-revalidate, private")
+    res.sendFile('index.html', {
+        root: staticPath
+    })
 })
 
-app.listen(3000, 'localhost', (err) => {
-    if (err) {
-        console.err(err)
-        return
-    }
+function nocache(req, res, next) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate')
+    res.header('Expires', '-1')
+    res.header('Pragma', 'no-cache')
+    next()
+}
 
-    console.log('Listening at http://localhost:3000')
+app.use('/', express.static(staticPath, {
+    maxage: 31557600
+}))
+
+const server = app.listen(process.env.PORT || 5000, function() {
+
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log(`Example app listening at http://${host}:${port}`)
+
 })
